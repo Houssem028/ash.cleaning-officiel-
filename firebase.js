2318310e4d5b7fc2edb5ab642437fb23b4c41169
Houssem028 <109749794+Houssem028@users.noreply.github.com>
2025-12-12 14:49:27 +0100

    Migrate Firebase imports to compat version

--- firebase.js
+++ firebase.js
@@ -1,26 +1,22 @@
 // firebase.js
+import firebase from "firebase/compat/app";
+import "firebase/compat/auth";
+import "firebase/compat/firestore";
 
-// استدعاء Firebase
-import { initializeApp } from "firebase/app";
-import { getFirestore } from "firebase/firestore";
-import { getAuth } from "firebase/auth";
-import { getAnalytics } from "firebase/analytics";
-
-// إعدادات مشروعك على Firebase
+// إعدادات Firebase
 const firebaseConfig = {
   apiKey: "AIzaSyAJxzFknapSh0fnPfIXwEEK3OijSKlqxco",
   authDomain: "admin-ash-f622c.firebaseapp.com",
   projectId: "admin-ash-f622c",
-  storageBucket: "admin-ash-f622c.firebasestorage.app",
+  storageBucket: "admin-ash-f622c.appspot.com",
   messagingSenderId: "76505742154",
   appId: "1:76505742154:web:8aa69b74f845f339cb1fb3",
   measurementId: "G-KVNZ1KM5ZZ"
 };
 
 // تهيئة Firebase
-const app = initializeApp(firebaseConfig);
-const analytics = getAnalytics(app);
+firebase.initializeApp(firebaseConfig);
 
-// تهيئة Firestore وAuth
-export const db = getFirestore(app);
-export const auth = getAuth(app);
+// التصدير لاستخدامه في صفحات أخرى
+export const auth = firebase.auth();
+export const db = firebase.firestore();
